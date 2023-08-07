f1d6b77e08a83d8a6cc3a7f1edda7a8d7736126c
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-08-07 03:30:24 +0200

    Started refactor to move towards BlueNBT

---
+++ src/main/java/com/technicjelle/bluemapofflineplayermarkers/models/NBTPlayer.java
@@ -0,0 +1,39 @@
+package com.technicjelle.bluemapofflineplayermarkers.models;
+
+import de.bluecolored.bluenbt.NBTName;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+@SuppressWarnings({"unused", "MismatchedReadAndWriteOfArray"})
+public class NBTPlayer {
+	@NBTName("playerGameType")
+	private int gameMode;
+
+	@NBTName("Pos")
+	private double[] position;
+
+	@NBTName("WorldUUIDLeast")
+	private long worldUUIDLeast;
+
+	@NBTName("WorldUUIDMost")
+	private long worldUUIDMost;
+
+	public GameMode getGameMode() {
+		//noinspection deprecation
+		return GameMode.getByValue(gameMode);
+	}
+
+	public @Nullable Location getLocation() {
+		UUID worldUUID = new UUID(worldUUIDMost, worldUUIDLeast);
+		World world = Bukkit.getWorld(worldUUID);
+
+		//World doesn't exist or position is broken
+		if (world == null || position.length != 3) return null;
+		return new Location(world, position[0], position[1], position[2]);
+	}
+}
